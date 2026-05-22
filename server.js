000e725bddc4eb367b18865ac6e347c17d841632
Ayushman050804 <ayushman.mukherjee05@gmail.com>
2026-05-23 00:44:20 +0530

    Linked medical records route in server.js

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/appointments', require('./routes/appointments')); 
+app.use('/api/appointments', require('./routes/appointments'));
+app.use('/api/records', require('./routes/records'));
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
